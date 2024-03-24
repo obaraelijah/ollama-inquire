fbc6d111e5676570a76325f69a9f1f89e1ed7315
obaraelijah <elijahobara357@gmail.com>
2024-03-24 11:18:44 +0300

    chore: cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use spinners_rs::{Spinner, Spinners};
+use std::env;
+use std::thread;
+use std::process::{Command, Output, Stdio};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 1 && args[1] == "--help" {
+        print_help();
+        return;
+    }
+
+    if args.len() >= 1 && args[1] == "--version" {
+        println!("ollama-inquire version: {}", env!("CARGO_PKG_VERSION"))
+    }
+    
+    let mut model = String::from("mistrial");
+    let mut question = String::from("Please enter your question: ");
+
+    let mut args_iter = args.iter().skip(1);
+    let model_specified = false;
+
+    
+
+}
+
+fn print_help() {
     unimplemented!()
 }
